4f729ad67a2a970e9df4ef76dc1b5e1122f45e06
Adelincllljy <adelincll@126.com>
2018-12-12 15:04:00 +0800

    cll_1212_writeback

--- SameTPSGeneration_Demo/daos/WriteBack.py
+++ SameTPSGeneration_Demo/daos/WriteBack.py
@@ -3,4 +3,31 @@ import sys
 import os
 import os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from services.DataMatcher import DataMatcher
+from services.ExtractRuler import ExtractRuler
+from utils.ExtractLocationInfo import ExtractLocationInfo
+from services.RuleDecorater import RuleDecorater
+from services.RuleMerger import RuleMerger
 from interactions.Predict import Predict
+from models.Item import Item
+
+class WriteBack():
+    def newxml(self,itemlist[],filepath):
+        '''
+        itemlist[]：传入itemlist集合
+        filepath：传入的原型xml的完整路径
+        '''
+
+        path1 = os.path.abspath('..')
+        # print(path1)
+        rootdir = filepath
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+        count = 0
+        for item in root.findall('item'):
+            reponse_text=item.findall('response')
+            templist=itemlist[count].reponses
+            for i in range(len(reponse_text)):
+                reponse_text[i].text=templist[i]
+            tree.write(path1+'\\datas\\data\\new'+str(count)+'.xml')
+            count+=1
